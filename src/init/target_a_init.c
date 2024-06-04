d2774a3823b2ae738b58fed836d1f5bdb882ef27
Saliinger <88099624+Saliinger@users.noreply.github.com>
2024-06-04 16:45:55 +0200

    work normally still an issue with the parsing

--- src/init/target_a_init.c
+++ src/init/target_a_init.c
@@ -6,42 +6,44 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 20:25:06 by anoukan           #+#    #+#             */
-/*   Updated: 2024/06/04 14:58:33 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/06/04 16:38:57 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
+static void	calcul_a(t_stack *a, t_stack *b, long *match_diff)
+{
+	if (a->number > b->number)
+	{
+		if (a->number - b->number < *match_diff)
+		{
+			*match_diff = a->number - b->number;
+			a->target = b;
+		}
+	}
+}
+
 void	target_a_init(t_stack **a, t_stack **b)
 {
 	t_stack	*temp_a;
 	t_stack	*temp_b;
-	t_stack	*max_b;
-	t_stack	*min_b;
 	long	match_diff;
 
-	max_b = is_max(b);
-	min_b = is_min(b);
 	temp_a = *a;
 	while (temp_a)
 	{
 		temp_b = *b;
 		match_diff = LONG_MAX;
-		temp_a->target = max_b;
-		if (temp_a->number > max_b->number || temp_a->number < min_b->number)
-			temp_a->target = max_b;
+		temp_a->target = is_max(b);
+		if (temp_a->number > is_max(b)->number
+			|| temp_a->number < is_min(b)->number)
+			temp_a->target = is_max(b);
 		else
 		{
 			while (temp_b)
 			{
-				if (temp_a->number > temp_b->number)
-				{
-					if (temp_a->number - temp_b->number < match_diff)
-					{
-						match_diff = temp_a->number - temp_b->number;
-						temp_a->target = temp_b;
-					}
-				}
+				calcul_a(temp_a, temp_b, &match_diff);
 				temp_b = temp_b->next;
 			}
 		}
--- src/init/target_b_init.c
+++ src/init/target_b_init.c
@@ -6,42 +6,44 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 14:56:57 by anoukan           #+#    #+#             */
-/*   Updated: 2024/06/04 14:59:14 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/06/04 16:42:29 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
+static void	calcul_b(t_stack *a, t_stack *b, long *match_diff)
+{
+	if (a->number > b->number)
+	{
+		if (a->number - b->number < *match_diff)
+		{
+			*match_diff = a->number - b->number;
+			b->target = a;
+		}
+	}
+}
+
 void	target_b_init(t_stack **a, t_stack **b)
 {
 	t_stack	*temp_a;
 	t_stack	*temp_b;
-	t_stack	*max_a;
-	t_stack	*min_a;
 	long	match_diff;
 
-	max_a = is_max(a);
-	min_a = is_min(a);
 	temp_b = *b;
 	while (temp_b)
 	{
 		temp_a = *a;
 		match_diff = LONG_MAX;
-		temp_b->target = max_a;
-		if (temp_b->number > max_a->number || temp_b->number < min_a->number)
-			temp_b->target = min_a;
+		temp_b->target = is_max(a);
+		if (temp_b->number > is_max(a)->number
+			|| temp_b->number < is_min(a)->number)
+			temp_b->target = is_min(a);
 		else
 		{
 			while (temp_a)
 			{
-				if (temp_a->number > temp_b->number)
-				{
-					if (temp_a->number - temp_b->number < match_diff)
-					{
-						match_diff = temp_a->number - temp_b->number;
-						temp_b->target = temp_a;
-					}
-				}
+				calcul_b(temp_a, temp_b, &match_diff);
 				temp_a = temp_a->next;
 			}
 		}
