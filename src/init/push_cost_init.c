c75f9bf7e6a6c4e15fd73dc34722fcb83b6692a5
Saliinger <alexisnoukan@gmail.com>
2024-06-10 13:16:11 +0200

    norm and work

--- src/init/push_cost_init.c
+++ src/init/push_cost_init.c
@@ -12,40 +12,37 @@
 
 #include "../../includes/push_swap.h"
 
-static void	calcul_above(t_stack *node, int size_a, int size_b)
+void compute_push_cost(t_stack *node, int size_a, int size_b)
 {
-	if (node->target->above_median == 1)
-		node->push_cost += (size_a - node->index) + node->target->index;
-	else
-		node->push_cost += (size_a - node->index) + (size_b
-				- node->target->index);
+    node->push_cost = 1;
+    if (node->above_median == 1)
+    {
+        if (node->target->above_median == 1)
+            node->push_cost += node->index + node->target->index;
+        else
+            node->push_cost += node->index + (size_b - node->target->index);
+    }
+    else
+    {
+        if (node->target->above_median == 1)
+            node->push_cost += (size_a - node->index) + node->target->index;
+        else
+            node->push_cost += (size_a - node->index) + (size_b - node->target->index);
+    }
 }
 
-static void	calcul_under(t_stack *node, int size_a, int size_b)
+void push_cost_init(t_stack **a, t_stack **b)
 {
-	if (node->target->above_median == 1)
-		node->push_cost += (size_a - node->index) + node->target->index;
-	else
-		node->push_cost += (size_a - node->index) + (size_b
-				- node->target->index);
-}
+    t_stack *temp_a;
+    int size_a;
+    int size_b;
 
-void	push_cost_init(t_stack **a, t_stack **b)
-{
-	t_stack	*temp_a;
-	int		size_a;
-	int		size_b;
-
-	size_a = ft_lstsize2(*a);
-	size_b = ft_lstsize2(*b);
-	temp_a = *a;
-	while (temp_a)
-	{
-		temp_a->push_cost = 1;
-		if (temp_a->above_median == 1)
-			calcul_above(temp_a, size_a, size_b);
-		else
-			calcul_under(temp_a, size_a, size_b);
-		temp_a = temp_a->next;
-	}
+    size_a = ft_lstsize2(*a);
+    size_b = ft_lstsize2(*b);
+    temp_a = *a;
+    while (temp_a)
+    {
+        compute_push_cost(temp_a, size_a, size_b);
+        temp_a = temp_a->next;
+    }
 }
